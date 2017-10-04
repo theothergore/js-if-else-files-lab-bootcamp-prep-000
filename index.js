aaeb83ded20248b5c02a761300738af3b318369a
Jennifer Gore <jennifer@theothergore.com>
2017-10-04 16:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
@@ -10,4 +10,3 @@ if (name === "Susan") {
 // Don't worry about this
 module.exports = { name, height
 }
-
